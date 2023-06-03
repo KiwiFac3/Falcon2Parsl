dfb8ce72c1006ca325e71991765f55e34b219d6c
Mousa Abughosh <mabughosh@hpclab-9.rd.unr.edu>
2023-06-03 13:35:54 -0700

    added workers to Tests

--- Test/FTP.py
+++ Test/FTP.py
@@ -1,33 +1,43 @@
 import parsl
-from parsl import python_app, File, MonitoringHub
+from parsl import python_app, File
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
 from parsl.data_provider.data_manager import NoOpFileStaging, FTPSeparateTaskStaging, HTTPSeparateTaskStaging
+import time
 
-working_dir= '/home/mabughosh/mabughosh/data/receive'
+working_dir = '/data/mabughosh/Falcon2Parsl/data'
+
+@python_app
+def convert(input_file):
+    with open(input_file, 'r') as f:
+        f.read()
+        return input_file
 
 config = Config(
     executors=[
         HighThroughputExecutor(
             working_dir=working_dir,
-            storage_access=[NoOpFileStaging(), FTPSeparateTaskStaging(),
-                            HTTPSeparateTaskStaging()],
+            storage_access=[NoOpFileStaging(), FTPSeparateTaskStaging(), HTTPSeparateTaskStaging()],
+            max_workers=8  # Adjust the number of workers based on available resources
         ),
     ],
 )
 
 parsl.load(config)
 
+start_time = time.time()
 
-@python_app
-def read_file(url):
-    with open('/home/mabughosh/mabughosh/data/receive/data44.txt', 'r') as f:
-        content = f.read()
-        return content
+inputs = []
+for x in range(0, 100):
+    inputs.append(File('ftp://134.197.113.70/largefile' + str(x) + '.txt'))
 
+convert_tasks = []
+for name in inputs:
+    task = convert(name)
+    convert_tasks.append(task)
 
-url = "ftp://134.197.95.132/data44.txt"
-file = File(url)
+results = [task.result() for task in convert_tasks]
 
-content = read_file(file)
-print(content.result())
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Code ran in {elapsed_time:.2f} seconds")
--- Test/HTTP.py
+++ Test/HTTP.py
@@ -1,32 +1,43 @@
 import parsl
-from parsl import python_app, File, MonitoringHub
+from parsl import python_app, File
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
 from parsl.data_provider.data_manager import NoOpFileStaging, FTPSeparateTaskStaging, HTTPSeparateTaskStaging
+import time
 
-working_dir= '/home/mabughosh/mabughosh/data/receive'
+working_dir = '/data/mabughosh/Falcon2Parsl/data'
 
 @python_app
-def convert(inputs=[], outputs=[]):
-    with open('/home/mabughosh/mabughosh/data/receive/'+inputs.filename, 'r') as f:
+def convert(input_file):
+    with open(input_file, 'r') as f:
         f.read()
-        return inputs.filename
+        return input_file
 
 config = Config(
     executors=[
         HighThroughputExecutor(
             working_dir=working_dir,
-            storage_access=[NoOpFileStaging(), FTPSeparateTaskStaging(),
-                            HTTPSeparateTaskStaging()],
+            storage_access=[NoOpFileStaging(), FTPSeparateTaskStaging(), HTTPSeparateTaskStaging()],
+            max_workers=8  # Adjust the number of workers based on available resources
         ),
     ],
 )
 
 parsl.load(config)
 
+start_time = time.time()
+
 inputs = []
-inputs = File('http://134.197.95.132/data44.txt')
+for x in range(0, 100):
+    inputs.append(File('http://134.197.113.70/largefile' + str(x) + '.txt'))
 
-f = convert(inputs)
+convert_tasks = []
+for name in inputs:
+    task = convert(name)
+    convert_tasks.append(task)
 
-print(f.result())
+results = [task.result() for task in convert_tasks]
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Code ran in {elapsed_time:.2f} seconds")
